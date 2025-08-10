acc82651220ced3f5ba54a739beddcb610efd2df
Joe-Elkilani <yousefelkilani15112011@gmail.com>
2025-08-10 14:54:48 +0200

    Editing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,16 @@
 const navbar = document.getElementById("header");
+const topEl = document.getElementById("top");
 
 window.addEventListener("scroll", function () {
     if (window.scrollY > 100) {
         navbar.classList.add("scrolled");
     } else {
         navbar.classList.remove("scrolled");
+    }
+    if (window.scrollY > 800) {
+        topEl.style.display = "block";
+    } else {
+        topEl.style.display = "none";
     }
 });
 let typed = new Typed('#work', {
